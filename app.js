10f4c33057892b9e7cddfc9ea23503e4d8ca51e4
adirahman <kodingku@github.com>
2018-10-19 17:42:43 +0700

    upload config

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', indexRouter);
 app.use('/users', usersRouter);
 
-app.listen(process.env.PORT ||  5000, function () {
-    console.log("app running on port.", process.env.PORT ||  5000);
+app.listen(process.env.PORT ||  7000, function () {
+    console.log("app running on port.", process.env.PORT ||  7000);
 });
 
 module.exports = app;
