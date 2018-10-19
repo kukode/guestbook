c001a26e44deb46ace55b51aff18629b7e89dcad
adirahman <kodingku@github.com>
2018-10-19 08:35:48 +0700

    upload app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors')
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(process.env.PORT ||  5000, function () {
+    console.log("app running on port.", process.env.PORT ||  5000);
+});
+
+module.exports = app;
